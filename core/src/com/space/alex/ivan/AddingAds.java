9b5f100922f63658a8b8f117c311aa138c0fb540
Alexander1990 <alexivanov2890@mail.ru>
2021-04-02 00:51:39 +0300

    added class AddingAds

---
+++ core/src/com/space/alex/ivan/AddingAds.java
@@ -0,0 +1,9 @@
+package com.space.alex.ivan;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class AddingAds extends BaseActor {
+    public AddingAds(float x, float y, Stage s) {
+        super(x, y, s);
+    }
+}
